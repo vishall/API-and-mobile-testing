596c0aabda285fa42dfb15c8277656c1715c95ce
vishall <vishal.loke@springernature.com>
2020-02-21 22:03:45 +0000

    [VL] Updated dependencies structure and extended solution further to cover bonus scenario

--- src/test/java/stepdefinations/GetCapitalCityStepDefs.java
+++ src/test/java/stepdefinations/GetCapitalCityStepDefs.java
@@ -1,64 +1,68 @@
 package stepdefinations;
 
-import io.cucumber.core.gherkin.vintage.internal.gherkin.deps.com.google.gson.JsonObject;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import org.json.JSONArray;
-import org.json.JSONObject;
 import utils.PropertiesReader;
-
-import java.util.Properties;
-
 import static io.restassured.RestAssured.given;
-import static io.restassured.parsing.Parser.JSON;
-
 
 public class GetCapitalCityStepDefs {
-
-
 
     @Given("I hit the get call for the capital city")
     public void i_hit_the_get_call_for_the_capital_city() {
 
         RestAssured.baseURI = PropertiesReader.getProperty("config","baseUrl");
-        given().get();
 
+                              given().
+                                get();
     }
 
-
-    @Then("I get a response for respective capital city")
+    @And("I get a response for respective capital city")
     public void i_get_a_response_for_respective_capital_city() {
         ValidatableResponse response =  given()
-                .get()
-                .then()
-                .assertThat()
-                .statusCode(200);
+                                         .get()
+                                        .then()
+                                  .assertThat()
+                                  .statusCode(200);
     }
 
-
-    @Then("I extracted the capital city from the response")
+    @When("I extracted the capital city from the response")
     public void i_extracted_the_capital_city_from_the_response() {
-        Response response = given().get();
+        Response response = given()
+                             .get();
 
         JSONArray JSONResponseBody = new   JSONArray(response.body().asString());
         JSONResponseBody.getJSONObject(0).get("capital");
 
     }
 
-
     @Then("I display the response on the console")
     public void i_display_the_response_on_the_console() {
 
-        Response response = given().get();
-        JSONArray JSONResponseBody = new   JSONArray(response.body().asString());
-        String Capital = String.valueOf(JSONResponseBody.getJSONObject(0).get("capital"));
+        Response response = given()
+                             .get();
+        JSONArray jsonArray = new   JSONArray(response.body().asString());
+        String Capital = String.valueOf(jsonArray.getJSONObject(0).get("capital"));
         System.out.println("Capital city is: "+Capital);
     }
 
+    @Then("I verify {string} presence in the response")
+    public void i_verify_presence_in_the_response(String string) {
 
+        Response response = given()
+                             .get();
+
+        if(response.body().asString().contains(string)){
+            System.out.println("True, "+string+" is present in the response");
+        }
+        else
+            System.out.println("False, unfortunately "+string+" is not present in the response");
+
+    }
 
 }
--- src/test/resources/GetCapitalCity.feature
+++ src/test/resources/GetCapitalCity.feature
@@ -3,7 +3,11 @@ Feature: Verify the response for the capital city
 
   Scenario: Extract the name of the capital city from response
     Given I hit the get call for the capital city
-    Then I get a response for respective capital city
-    And I extracted the capital city from the response
-    Then I display the response on the console
+      And I get a response for respective capital city
+     When I extracted the capital city from the response
+     Then I display the response on the console
 
+  Scenario: Verify text presence in the response
+    Given I hit the get call for the capital city
+      And I get a response for respective capital city
+     Then I verify "British pound" presence in the response
